68fc5c18843071ffb5dd022e28c763212351facb
Dzmitry Novik <nowikmitya@yandex.ru>
2023-06-06 00:09:34 +0300

    app for testing homework #2

---
+++ SpringAppForSecondHomework/src/main/java/ru/novik/SpringAppForSecondHomework/SpringAppForSecondHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.novik.SpringAppForSecondHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppForSecondHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppForSecondHomeworkApplication.class, args);
+	}
+
+}
---
+++ SpringAppForSecondHomework/src/test/java/ru/novik/SpringAppForSecondHomework/SpringAppForSecondHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.novik.SpringAppForSecondHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppForSecondHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
